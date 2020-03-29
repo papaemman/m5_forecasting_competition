217a17693e69363943cca37402f351b6648ff1df
Panagiotis Papaemmanouil <papaemman.pan@gmail.com>
2020-03-30 00:37:44 +0300

    how to caclulate historical proportions for dis aggregation in lower levels

---
+++ cases/calc_historical_proportions_for_top_down.R
@@ -0,0 +1,35 @@
+##############################################################
+#                                                            # 
+# Calculate historical proportion for TOP-DOWN approaches    #
+#                                                            #
+##############################################################
+
+# If I use top-down forecasting approaches,
+# meaning that I make forecastings for time series in aggregation level greater than 12, 
+# then I have to caclulate historical proportions gor every time series in lower aggregation level.
+
+
+# Import datasets
+sales <- read.csv("data/raw/sales_train_validation.csv")
+time_series_b <- readRDS("data/processed/time_series_b.rds")
+
+
+## Aggregation level 1: Count the total sales for each day for all products in all stores ----
+
+insample_top <- ts(as.numeric(colSums(sales[,7:ncol(sales)])), frequency = 7)
+
+# How to calculate proportions:
+# Divide the total sales for each product for the last 28 days, with the total sales of the last 28 days
+proportions <- unlist(lapply(X = c(1:length(time_series_b)), 
+                             FUN = function(x) {
+                               cat("\r",paste("ts id:", x))
+                               sum(tail(as.numeric(sales[x,7:ncol(sales)]),28))/sum(tail(insample_top, 28))
+                             }))
+
+head(proportions)
+str(proportions)
+
+saveRDS(object = proportions, file = "data/processed/historical_proportions_level_1.rds")
+
+
+
