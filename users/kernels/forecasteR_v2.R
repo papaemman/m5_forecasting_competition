d2dfd6d93abdd6a4def14f79bc919384c924f3ec
Panagiotis Papaemmanouil <panagiotis.papaemmanouil@medoid.ai>
2020-04-13 07:19:18 +0000

    kernels and ideas

--- users/kernels/forecasteR_v2.R
+++ users/kernels/forecasteR_v2.R
@@ -1,10 +1,10 @@
 ########################################
 #                                      #
-#  M5 ForecasteR [Score = 0.57330]     #
+#  M5 ForecasteR v2 [Score = 0.51084]  #
 #                                      #
 ########################################
 
-## Kernel's link: https://www.kaggle.com/kailex/m5-forecaster-0-57330 
+## Kernel's link: https://www.kaggle.com/kailex/m5-forecaster-v2 
 
 ## // My Notes //
 # General Approach: 
@@ -14,9 +14,12 @@
 
 
 
-## 01. Load data and packages ----
+## 00. Load packages ----
+
+library(data.table)
 library(lightgbm)
 
+# 01. Define parameters -----
 set.seed(0)
 
 h <- 28 
@@ -24,58 +27,53 @@ max_lags <- 366
 tr_last <- 1913
 fday <- as.IDate("2016-04-25") 
 
-#---------------------------
-cat("Creating auxiliary functions...\n")
-
 free <- function() invisible(gc())
+
 
 create_dt <- function(is_train = TRUE, nrows = Inf) {
   
-  prices <- fread("../input/m5-forecasting-accuracy/sell_prices.csv")
-  cal <- fread("../input/m5-forecasting-accuracy/calendar.csv")
-  cal[, `:=`(date = as.IDate(date, format="%Y-%m-%d"),
-             is_weekend = as.integer(weekday %chin% c("Saturday", "Sunday")))]
-  
   if (is_train) {
-    dt <- fread("../input/m5-forecasting-accuracy/sales_train_validation.csv", nrows = nrows)
+    dt <- fread("data/raw/sales_train_validation.csv", nrows = nrows)
+    
   } else {
-    dt <- fread("../input/m5-forecasting-accuracy/sales_train_validation.csv", nrows = nrows,
+    
+    dt <- fread("data/raw/sales_train_validation.csv", nrows = nrows,
                 drop = paste0("d_", 1:(tr_last-max_lags)))
     dt[, paste0("d_", (tr_last+1):(tr_last+2*h)) := NA_real_]
   }
+  
   
   dt <- melt(dt,
              measure.vars = patterns("^d_"),
              variable.name = "d",
              value.name = "sales")
   
-  dt <- dt[cal, `:=`(date = i.date, 
-                     is_weekend = i.is_weekend,
+  cal <- fread("../input/m5-forecasting-accuracy/calendar.csv")
+  dt <- dt[cal, `:=`(date = as.IDate(i.date, format="%Y-%m-%d"), 
                      wm_yr_wk = i.wm_yr_wk,
                      event_name_1 = i.event_name_1,
+                     event_name_2 = i.event_name_2,
                      snap_CA = i.snap_CA,
                      snap_TX = i.snap_TX,
                      snap_WI = i.snap_WI), on = "d"]
   
+  prices <- fread("../input/m5-forecasting-accuracy/sell_prices.csv")
   dt[prices, sell_price := i.sell_price, on = c("store_id", "item_id", "wm_yr_wk")]
 }
 
 create_fea <- function(dt) {
+  dt[, `:=`(d = NULL,
+            wm_yr_wk = NULL)]
   
-  lag <- c(7, 28, 29)
-  dt[, (paste0("lag_", lag)) := shift(.SD, lag), .SDcols = "sales", by = "id"]
+  lag <- c(7, 28)
+  lag_cols <- paste0("lag_", lag)
+  dt[, (lag_cols) := shift(.SD, lag), by = id, .SDcols = "sales"]
   
-  win <- c(7, 30, 90, 180)
-  dt[, (paste0("roll_mean_28_", win)) := frollmean(lag_28, win, na.rm = TRUE), by = "id"]
+  win <- c(7, 28)
+  roll_cols <- paste0("rmean_", t(outer(lag, win, paste, sep="_")))
+  dt[, (roll_cols) := frollmean(.SD, win, na.rm = TRUE), by = id, .SDcols = lag_cols]   
   
-  win <- c(28)
-  dt[, (paste0("roll_max_28_", win)) := frollapply(lag_28, win, max), by = "id"]
-  dt[, (paste0("roll_var_28_", win)) := frollapply(lag_28, win, var), by = "id"]
-  
-  dt[, price_change_1 := sell_price / shift(sell_price) - 1, by = "id"]
-  dt[, price_change_365 := sell_price / frollapply(shift(sell_price), 365, max) - 1, by = "id"]
-  
-  cols <- c("item_id", "state_id", "dept_id", "cat_id", "event_name_1")   
+  cols <- c("item_id", "store_id", "state_id", "dept_id", "cat_id", "event_name_1", "event_name_2")   
   dt[, (cols) := lapply(.SD, function(x) as.integer(factor(x))), .SDcols = cols]
   
   dt[, `:=`(wday = wday(date),
@@ -83,14 +81,11 @@ create_fea <- function(dt) {
             week = week(date),
             month = month(date),
             quarter = quarter(date),
-            year = year(date),
-            store_id = NULL,
-            d = NULL,
-            wm_yr_wk = NULL)]
+            year = year(date))]
 }
 
 #---------------------------
-cat("Processing datasets...\n")
+cat("Preprocessing datasets...\n")
 
 tr <- create_dt()
 free()
@@ -100,23 +95,18 @@ free()
 
 tr <- na.omit(tr)
 y <- tr$sales
-
-idx <- tr[date <= max(date)-h, which = TRUE] 
-
 tr[, c("id", "sales", "date") := NULL]
 free()
 
 tr <- data.matrix(tr)
 free()
 
-cats <- c("item_id", "state_id", "dept_id", "cat_id",
-          "wday", "mday", "week", "month", "quarter", "year", "is_weekend",
+cats <- c("item_id", "store_id", "state_id", "dept_id", "cat_id", 
+          "wday", "mday", "week", "month", "quarter", "year", 
           "snap_CA", "snap_TX", "snap_WI")
+xtr <- lgb.Dataset(tr, label = y, categorical_feature = cats)
 
-xtr <- lgb.Dataset(tr[idx, ], label = y[idx], categorical_feature = cats)
-xval <- lgb.Dataset(tr[-idx, ], label = y[-idx], categorical_feature = cats)
-
-rm(tr, y, idx)
+rm(tr, y, cats)
 free()
 
 #---------------------------
@@ -131,18 +121,12 @@ p <- list(objective = "poisson",
           bagging_freq = 1,
           lambda_l2 = 0.1,
           nthread = 4)
+m_lgb <- lgb.train(p, xtr, 4000)
 
-m_lgb <- lgb.train(params = p,
-                   data = xtr,
-                   nrounds = 2000,
-                   valids = list(valid = xval),
-                   early_stopping_rounds = 400,
-                   eval_freq = 200)
+imp <- lgb.importance(m_lgb)
+lgb.plot.importance(imp, 20, cex = 0.9) 
 
-cat("Best score:", m_lgb$best_score, "at", m_lgb$best_iter, "iteration")                         
-lgb.plot.importance(lgb.importance(m_lgb), 20)
-
-rm(xtr, xval, p)
+rm(xtr, p)
 free()
 
 #---------------------------
@@ -155,7 +139,7 @@ for (day in as.list(seq(fday, length.out = 2*h, by = "day"))){
   tst <- te[date >= day - max_lags & date <= day]
   create_fea(tst)
   tst <- data.matrix(tst[date == day][, c("id", "sales", "date") := NULL])
-  te[date == day, sales := predict(m_lgb, tst)]
+  te[date == day, sales := 1.02*predict(m_lgb, tst)] # magic multiplier by kyakovlev
 }
 
 te[date >= fday
