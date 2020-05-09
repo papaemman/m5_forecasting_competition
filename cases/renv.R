d4a8fe042d8cc35339d2931c7980c114a6ab600b
fachantidis <anestis@medoid.ai>
2020-05-09 12:17:43 +0000

    renv

--- cases/renv.R
+++ cases/renv.R
@@ -27,3 +27,12 @@ renv::settings$ignored.packages()
 renv::settings$external.libraries()
 renv::settings$use.cache()
 renv::settings$package.dependency.fields()
+
+renv::install("microsoft/lightgbm")
+
+renv::rebuild(library = "lightgbm")
+
+renv::paths$root()
+renv::paths$library()
+renv::paths$cache()
+
